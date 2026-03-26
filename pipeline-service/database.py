a2672af177b7c3aca948876533810ed9291981c3
Sarthak Goyal <85985837+Sarthak-81@users.noreply.github.com>
2026-03-26 14:39:42 +0530

    Update database.py
    Indentation issue

--- pipeline-service/database.py
+++ pipeline-service/database.py
@@ -5,19 +5,19 @@ import time
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# 🔥 Retry DB connection
+# Retry DB connection
 for i in range(10):
     try:
         engine = create_engine(DATABASE_URL)
         connection = engine.connect()
         connection.close()
-        print("✅ Database connected")
+        print("Database connected")
         break
     except Exception as e:
-        print("⏳ Waiting for DB...")
+        print("Waiting for DB...")
         time.sleep(3)
 else:
-    raise Exception("❌ Could not connect to DB")
+    raise Exception("Could not connect to DB")
 
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
